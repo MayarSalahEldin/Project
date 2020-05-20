2d75964fcd88417bad9e355579c8a09324339374
Mayar Salah ELDin <mayar_salah13@yahoo.com>
2020-05-20 23:47:41 +0200

    mayar1

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Myapp
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins 'nausheen-fatima.github.io'
+        origins 'MayarSalahEldin.github.io'
         resource '/api/*',
           :headers => :any,
           :methods => [:get, :post, :put, :delete, :options]
