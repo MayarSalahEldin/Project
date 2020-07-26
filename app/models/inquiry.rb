3dca155a1d544a0116c6c495221e5d54d6faddeb
Mayar Salah ELDin <mayar_salah13@yahoo.com>
2020-07-27 01:41:02 +0200

    s1

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,3 +1,7 @@
 class Inquiry < ActiveRecord::Base
+	include Mongoid::Document
+  include Mongoid::Timestamps
+   field :subject, type: String
+  field :description, type: String
   include Protectable
 end
--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,4 +1,9 @@
 class Thing < ActiveRecord::Base
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :name, type: String
+  field :description, type: String
+  field :notes, type: String
   include Protectable
   validates :name, :presence=>true
 
