5b31aee2d3b50ab4734a8c3f71140941a701494b
Mayar Salah ELDin <mayar_salah13@yahoo.com>
2020-07-27 14:49:11 +0200

    mayar

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   field :nickname, type: String
   field :image, type: String
   field :email, type: String
-  field :tokens, type: String
+#  field :tokens, type: String
   field :created_at, type: String
   field :updated_at, type: String
   
